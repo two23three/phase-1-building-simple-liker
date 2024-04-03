9f253c03c33f30ccf7fc6ae06066a70dbeb8c9a0
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-04-03 14:11:13 +0300

    liker

--- main.js
+++ main.js
@@ -4,7 +4,35 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let heartIcons = document.querySelectorAll(".like-glyph")
 
+
+heartIcons.forEach(item => {
+  item.addEventListener('click', likeHandler)
+})
+
+function likeHandler(e) {
+
+  mimicServerCall()
+  .then(data => {
+    if(data && e.target.innerHTML === EMPTY_HEART){
+      e.target.className = 'activated-heart'
+      e.target.innerHTML = FULL_HEART
+    }else if (data || e.target.innerHTML === FULL_HEART){
+      e.target.className = 'like-glyph'
+      e.target.innerHTML = EMPTY_HEART
+    }
+  })
+  .catch(function(error){
+    const modal = document.querySelector('div#modal')
+    modal.className = ''
+    const p = document.querySelector(['p#modal-message'])
+    p.innerHTML = error
+    setTimeout(function () {
+      return modal.className = 'hidden'
+    }, 3000)
+  })
+}
 
 
 //------------------------------------------------------------------------------
